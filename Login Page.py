ea592f5598d476e8cc724c95a46ba1ec1229bdad
E137299 <david.yin@austinisd.org>
2021-10-31 23:04:26 -0500

    Initial Commit

---
+++ Login Page.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+######### MODEL (Data)#########
+
+######### CONTROLLER (Widgets Responsible for Manipulating Data) #########
+
+######### VIEW (Display Current State of Data) #########
